a0fcb3ec1bd6d752ad1fd805629709afe42cacb1
Trisha2111 <87272160+Trisha2111@users.noreply.github.com>
2023-11-18 14:00:14 +0530

    Add files via upload

---
+++ brute_force password pdf__Reference Code.py
@@ -0,0 +1,30 @@
+import PyPDF2 as pd
+filename = input('Path to the file: ')
+filename = filename.strip()
+file = open(filename,'rb')
+pdfReader = pd.PdfReader(file)
+
+tried = 0
+
+if not pdfReader.is_encrypted:
+    print('The file is not password protected! You can successfully open it!')
+
+else:
+    wordListFile = open('wordlist.txt', 'r',errors='ignore')
+    body = wordListFile.read().lower()
+    words = body.split('\n')
+
+    for i in range(len(words)):
+        word=words[i]
+        print("Trying to decode password by:",word)
+        result=pdfReader.decrypt(word)
+        if result==1:
+            print("Success!the password is:",word)
+            break
+        elif result==0:
+            tried+=1
+            print("passwords tried:",tried)
+            continue
+            
+
+
